ae00ee3adc19c76fa1d3b2eb9fa9663a85027a68
TheJMG12 <ngosine83@gmail.com>
2017-03-28 15:05:48 -0400

    Create Music_sorting.py

---
+++ Music_sorting.py
@@ -0,0 +1,46 @@
+class Music():
+    def _init_(self):
+        self.artist=''
+        self.genre=''
+        self.album=''
+        self.most_played=''
+        self.year=''
+
+cd = Music()
+cd.artist='Travis Scott'
+cd.genre='Rap'
+cd.album='Rodeo'
+cd.most_played='Oh My Dis Side'
+cd.year='2015'
+print('')
+print('Song playing - ', cd.most_played)
+print('Artist - ',cd.artist)
+print('Album - ',cd.album, 'From', cd.year,)
+
+vinyl = Music()
+vinyl.artist='Travis Scott'
+vinyl.genre='Rap'
+vinyl.album='Birds in the trap sing mcknight'
+vinyl.most_played='goosebumps'
+vinyl.year='2016'
+print('')
+print('Song playing - ', vinyl.most_played)
+print('Artist - ', vinyl.artist)
+print('Album - ', vinyl.album, 'From', vinyl.year,)
+
+mp3 =  Music()
+mp3.artist='Migos'
+mp3.genre='Rap'
+mp3.album='Culture'
+mp3.most_played='Kelly Price (feat. Travis Scott)'
+mp3.year='2017'
+print('')
+print('Song playing - ', mp3.most_played)
+print('Artist - ', mp3.artist)
+print('Album - ', mp3.album, 'From', mp3.year,)
+
+
+
+      
+        
+
